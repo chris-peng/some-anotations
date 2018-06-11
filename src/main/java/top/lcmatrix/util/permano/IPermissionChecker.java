d7fadb79493fc09ce61abf26faadbd1ca1bc2a14
penghaifeng <penghaifeng@foxmail.com>
2018-06-11 10:28:17 +0800

    删除失效的文件；优化部分注释

--- src/main/java/top/lcmatrix/util/permano/IPermissionChecker.java
+++ src/main/java/top/lcmatrix/util/permano/IPermissionChecker.java
@@ -15,7 +15,7 @@ public interface IPermissionChecker {
 	public void onAccept();
 	
 	/**
-	 * 最终不通过时将执行该方法，然后原方法将不被执行，而直接返回null，也可以在该方法中抛出异常以中止执行
+	 * 最终不通过时将执行该方法，然后原方法将不被执行，而直接返回null。可以在该方法中抛出异常让外层捕获
 	 * @return
 	 */
 	public void onDeny();
